6b788b3353cfff59b71ceb80ded03487b444c91e
yogesh <yogee1389@gmail.com>
2019-05-07 11:22:54 +0530

    First Commit

---
+++ src/main/java/com/example/microservice/CurrencyExchangeService/Controller/CurrencyController.java
@@ -0,0 +1,30 @@
+package com.example.microservice.CurrencyExchangeService.Controller;
+
+import java.math.BigDecimal;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.microservice.CurrencyExchangeService.bean.ExchangeValue;
+import com.example.microservice.CurrencyExchangeService.repositories.ExchangeValueRepository;
+@RestController
+public class CurrencyController {
+	
+	@Autowired
+	private Environment environment;
+	
+	@Autowired
+	private ExchangeValueRepository repository;
+	
+	@GetMapping(path="/exchange-service/from/{from}/to/{to}")
+	public ExchangeValue exchange(@PathVariable String from,@PathVariable String to){
+		ExchangeValue exchange = repository.findByFromAndTo(from, to);
+		//System.out.println(environment.getProperty("local.server.port"));
+		//exchange.setPort(Integer.parseInt(environment.getProperty("local.server.port")));
+		return exchange ;
+	}
+
+}
---
+++ src/main/resources/data.sql
@@ -0,0 +1,3 @@
+insert into exchange_value(id,currency_from,currency_to,conversion_multiple,port) values(1,'USD','INR',65,0);
+insert into exchange_value(id,currency_from,currency_to,conversion_multiple,port) values(2,'USD','EUR',70,0);
+insert into exchange_value(id,currency_from,currency_to,conversion_multiple,port) values(3,'AUS','INR',25,0);
